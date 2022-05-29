170acbab8cb40774abef7192e171f5030c788e82
Owen Newlove <owen.newlove@gmail.com>
2022-05-29 17:23:19 -0400

    used question.textContent to change the text within the h2 tag, then added when the start quiz button is clicked the start button and p element will be removed. Then a ul with 4 li's will be created with a button being created in each li item. Then the buttons will be filled with text. Then set the margin, list style, and text align for the ul. Then added padding to each li item

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,19 @@ var timerEl = document.getElementById('timer');
 var startEl = document.querySelector(".startBtn");
 
 var page1 = document.querySelector(".firstPage");
-var questions = document.querySelector(".question")
-var answers = document.querySelector(".answers")
+var questions = document.querySelector(".question");
+var statement = document.getElementById("startStatement")
 
-
-
-
-
+var listEl = document.createElement("ul");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+var button1 = document.createElement("button")
+var button2 = document.createElement("button")
+var button3 = document.createElement("button")
+var button4 = document.createElement("button")
     
-
-
-
 
 function countdown() {
     var timeLeft = 75;
@@ -40,6 +42,34 @@ function countdown() {
 
 startEl.addEventListener("click", function() {
     console.log("click")
+
+    question.textContent = "Commonly used data types do NOT include:"
+    startEl.remove();
+    statement.remove();
+
+    page1.appendChild(listEl);
+    listEl.appendChild(li1);
+    listEl.appendChild(li2);
+    listEl.appendChild(li3);
+    listEl.appendChild(li4);
+    li1.appendChild(button1);
+    li2.appendChild(button2);
+    li3.appendChild(button3);
+    li4.appendChild(button4);
+    listEl.setAttribute("style", "list-style: none; margin-top: 60px; text-align: start;");
+    button1.textContent = "boolean"
+    button2.textContent = "strings"
+    button3.textContent = "alerts"
+    button4.textContent = "numbers"
+    li1.setAttribute("style", "padding: 10px")
+    li2.setAttribute("style", "padding: 10px")
+    li3.setAttribute("style", "padding: 10px")
+    li4.setAttribute("style", "padding: 10px")
+
+
+
+
+
     countdown();
 
   
