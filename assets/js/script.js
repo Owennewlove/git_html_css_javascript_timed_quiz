5d405307f818088b6180a191954222dabcaa2944
Owen Newlove <owen.newlove@gmail.com>
2022-05-29 16:52:19 -0400

    I added new variables to call upon items in the html and then went a direction that I think will take me more time so I decided to use it as a comment in the java script to keep the ideas in my head but will continue down a different path of appending the page on clicks instead of trying to make things hidden and visible

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 
-timerEl = document.getElementById('timer');
-startEl = document.querySelector(".startBtn")
+var timerEl = document.getElementById('timer');
+var startEl = document.querySelector(".startBtn");
+
+var page1 = document.querySelector(".firstPage");
+var questions = document.querySelector(".question")
+var answers = document.querySelector(".answers")
+
+
+
+
+
+    
+
+
+
 
 function countdown() {
     var timeLeft = 75;
@@ -29,5 +42,21 @@ startEl.addEventListener("click", function() {
     console.log("click")
     countdown();
 
+  
+
 });
 
+
+
+      /* <section class="secondPage" data-state="hidden" >
+            
+            <h2 class = "question">Commonly used data types do <bold>NOT</bold> include:</h2>
+
+            <ul class = "answers">
+                <li><button>boolean</button></li>
+                <li><button>strings</button></li>
+                <li><button>alerts</button></li>
+                <li><button>numbers</button></li>
+            </ul>
+
+        </section> */
