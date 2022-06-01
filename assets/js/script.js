59ea551404169dd00b03e65dd1dc7d353f672fbe
Owen Newlove <owen.newlove@gmail.com>
2022-06-01 14:55:59 -0400

    created a build quiz function that will call upon the questions and answers within the array

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,36 @@ var setEigthButton = fourthButton.setAttribute("id", "button8");
 var checkTimer = 3;
 var timeLeft = 75;
 
+function buildQuiz(){
 
+    const output = [];
+
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+            const answers = [];
+
+            for (letter in currentQuestion.answers){
+
+                answers.push(
+                    `<label>
+                        <input type = "radio" name = "question${questionNumber}" value = "${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+                    
+                );
+            }
+
+            output.push(
+                `<div class = "question"> ${currentQuestion.question} </div>
+                <div class = "answers"> ${answers.join('')} </div>`
+            );
+        }
+    );
+
+    firstPage.innerHTML = output.join('');
+}
 
 const myQuestions = [
     {
