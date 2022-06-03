9c30a4a8af31c0257b52917ccdf66c3e2d45b811
Owen Newlove <owen.newlove@gmail.com>
2022-06-03 15:04:46 -0400

    deleted old code that had been commented because it was wrong but could still be referenced when I was creating the correct code

--- assets/js/script.js
+++ assets/js/script.js
@@ -374,7 +374,7 @@ tenthButton.addEventListener('click', function () {
 function renderScores() {
 
 
-    // Render a new li for each todo
+    // Render a new li for each score
     for (var i = 0; i < scores.length; i++) {
         var score = scores[i];
 
@@ -388,15 +388,15 @@ function renderScores() {
 }
 
 function init() {
-    // Get stored todos from localStorage
+    // Get stored scores from localStorage
     var storedScores = JSON.parse(localStorage.getItem("scores"));
 
-    // If todos were retrieved from localStorage, update the todos array to it
+    // If scores were retrieved from localStorage, update the scores array to it
     if (storedScores !== null) {
         scores = storedScores;
     }
 
-    // This is a helper function that will render todos to the DOM
+    // This is a helper function that will render scores to the DOM
     renderScores();
 }
 
@@ -409,16 +409,16 @@ submitEl.addEventListener("click", function (event) {
 
     var initialsText = initialsEl.value.trim();
 
-    // Return from function early if submitted todoText is blank
+    // Return from function early if submitted initialsText is blank
     if (initialsText === "") {
         return;
     }
 
-    // Add new initials and score to todos array, clear the input
+
     scores.push(initialsText + "    Score:     " + timeLeft);
     initialsEl.value = "";
 
-    // Store updated scores in localStorage, re-render the list
+
     storeScores();
     renderScores();
 });
@@ -457,226 +457,3 @@ goBackEl.addEventListener("click", function () {
 
 
 
-
-/*firstButton.addEventListener("click", function () {
-
-     console.log("button 1 clicked")
-     var correctInterval = setInterval(function () {
-         if (checkTimer > 0) {
-             check.textContent = "Correct";
-             checkTimer--;
-         }
-         else {
-             check.textContent = "";
-         }
-
-     }, 1000);
-
-     checkTimer = 3
-
-      secondButton.addEventListener("click", function () {
-
-     var checkTimer = 3;
-     console.log("button 2 clicked")
-     var wrongInterval = setInterval(function () {
-         if (checkTimer > 0) {
-             check.textContent = "Wrong";
-             checkTimer--;
-         }
-         else {
-             check.textContent = "";
-         }
-
-     }, 1000);
-     checkTimer = 3
-     timeLeft -= 15;
-
- });
-
- thirdButton.addEventListener("click", function () {
-
-     console.log("button 1 clicked")
-     var wrongInterval = setInterval(function () {
-         if (checkTimer > 0) {
-             check.textContent = "Wrong";
-             checkTimer--;
-         }
-         else {
-             check.textContent = "";
-         }
-
-     }, 1000);
-
-     timeLeft -= 15;
-     checkTimer = 3;
-
- });
-
- fourthButton.addEventListener("click", function () {
-
-     console.log("button 4 clicked")
-
-     var wrongInterval = setInterval(function () {
-         if (checkTimer > 0) {
-             check.textContent = "Wrong";
-             checkTimer--;
-         }
-         else {
-             check.textContent = "";
-         }
-
-     }, 1000);
-
-     timeLeft -= 15;
-     checkTimer = 3;
-
- });
-
-
-
-
-     setFifthButton;
-     setSixthButton;
-     setSeventhButton;
-     setEigthButton;
-
-
-
-
-
-
-
-fifthButton.textContent = "quotes"
-sixthButton.textContent = "curly brackets"
-seventhButton.textContent = "parentheses"
-eigthButton.textContent = "square brackets"
-
-
-fifthButton.addEventListener("clicked", function () {
-
- var wrongInterval = setInterval(function () {
-     if (checkTimer > 0) {
-         check.textContent = "Wrong";
-         checkTimer--;
-     }
-     else {
-         check.textContent = "";
-     }
-
- }, 1000);
- console.log("fifth button clicked")
-
- checkTimer = 3;
- timeLeft -= 15;
-
-
-
-
-})*/
-
-
-
-
-
-
-
-
-
-
-
-/*    if (ev.currentTarget.id = "button2") {
-            wrongInterval;
-            console.log("button 2 clicked")
-        }
-
-        if (ev.currentTarget.id = "button3") {
-            wrongInterval;
-            console.log("button 3 clicked")
-        }
-
-        if (ev.currentTarget.id = "button4") {
-            wrongInterval;
-            console.log("button 4 clicked")
-        }  */
-
-/* <section class="secondPage" data-state="hidden" >
-            
-<h2 class = "question">Commonly used data types do <bold>NOT</bold> include:</h2>
-
-<ul class = "answers">
-<li><button>boolean</button></li>
-<li><button>strings</button></li>
-<li><button>alerts</button></li>
-<li><button>numbers</button></li>
-</ul>
- </section> */
-
-
-/*const myQuestions = [
-   {
-       question: "Commonly used data types DO NOT include:",
-
-       answers: {
-           1: "strings",
-           2: "boolean",
-           3: "alerts",
-           4: "numbers"
-       },
-   },
-
-   {
-       question: "The condition in an if/else statement is enclosed within ______. ",
-
-       answers: {
-
-           a: "quotes",
-           b: "curly brackets",
-           c: "parentheses",
-           d: "square brackets"
-
-       },
-   },
-
-   {
-
-       question: "Arrays in JavaScript can be used to store _____.",
-
-       answers: {
-
-           a: "numbers and strings",
-           b: "other arrays",
-           c: "booleans",
-           d: "all of the above"
-       },
-
-   },
-
-   {
-       question: "String values must be enclosed within ______ when being assigned to variables.",
-
-       answers: {
-           a: "commas",
-           b: "curly brackets",
-           c: "quotes",
-           d: "parentheses"
-       },
-   },
-
-   {
-
-       question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-
-       answers: {
-
-           a: "JavaScript",
-           b: "terminal/bash",
-           c: "for loops",
-           d: "console log"
-       }
-   }
-
-
-
-
-
-]*/
